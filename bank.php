6271375b4b20c4ba34c7f04f29d9ee9b492ecd5e
embtauber/em <embenjamintauber@gmail.com/em>
2013-08-21 17:29:39 -0400

    commit

--- bank.php
+++ bank.php
@@ -1,13 +1,23 @@
-<?php 
+<?php
+$program = new sign_up; 
 class sign_up{
 
   public function __construct(){
         if($_REQUEST == NULL){
+        	$this->main_page();
+        }
+        if($_REQUEST['page'] == 'sign_up'){
         	$this->sign_up_form();
         }
-        elseif($_REQUEST['page'] == 'log_in'){
+        if($_REQUEST['page'] == 'sign_in'){
         	$this->log_in_form();
+
         }
+  }     
+  function main_page(){
+  	echo 'hello welcome to my program <br>';
+  	echo '<a href="./bank.php?page=sign_up">Click to sign up</a>';
+  	echo '<a href="./bank.php?page=sign_in">Click if you are a member, to sign in</a>';
   }
   function sign_up_form(){
 	$form1 = new form;
@@ -17,5 +27,8 @@ class sign_up{
   	$form2 = new form;
   	$form2->log_in_form();
   }
+  
+  	
+  
 }
 ?>
