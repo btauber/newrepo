d82abfecfd97c69d73ba82b63a339b235db831c2
embtauber/em <embenjamintauber@gmail.com/em>
2013-08-27 19:43:21 -0400

    bank homework

--- bank.php
+++ bank.php
@@ -1,4 +1,4 @@
-<?php
+<?php  
 $program = new MyBank; 
 class MyBank{
 
@@ -23,12 +23,25 @@ class MyBank{
   	$form2 = new form;
   	$form2->log_in_form();
   }
+   
   function sign_up_verify() {
-  	$verify = new sign_up_verify;
-  	$verify->write_csv();
- } 
+	
+	//print_r($_POST);
+        $row = 0;
+        foreach($_POST as $key => $val){
+           if (empty($val)){
+           $row++ ;
+            }
+         }
+          if($row  !==0){
+          echo 'Please, fill out this form completly,Thank';
+          }  else {
+        $verify = new sign_up_verify; echo 'GOT IT';
+        $verify->write_csv($_POST);
+        $verify->read_signUpForm();
+             }
   	
-  
+  } 
 }
 
 
@@ -46,39 +59,65 @@ class form{
 	  		<LABEL for="password">Please Create a Password:</LABEL>
 	  		<INPUT type="text" name="password"><BR>
 	  		<INPUT type="submit" value="Send"> <INPUT type="reset">
-	  		</FORM';
+	  		</FORM'; echo '<a href="./bank.php?page=sign_in">Click if you are a member, to sign in</a>';
 	  echo "$form1";
-	// print_r($form1);
 	}
 	public function log_in_form(){
-	  $form2 = 'LogInForm';
+	  $form2 = '<br>
+	  		<FORM action="./bank.php?page=sign_in" method="post">
+	  		<LABEL for="user_name">User name: </LABEL>
+	  		<INPUT type="text" name="user_name"><BR>
+	  		<LABEL for="password">Password: </LABEL>
+	  		<INPUT type="text" name="password"><BR>
+	  		<INPUT type="submit" value="Send"> <INPUT type="reset">
+	  		</FORM'; echo '<a href="./bank.php?page=sign_up">Click to sign up</a>';
 	  echo "$form2";
 		
 	}
-	
-}
-
-class sign_up_verify{
-	public function __construct(){
-		session_start();
-		print_r($_POST);
 		
-	}
-	public function write_csv(){
-		$list = array(
-		$_POST['first_name'],
-		$_POST['last_name'],
-		$_POST['user_name'],
-		$_POST['password']
-		);
+}
+class sign_up_verify{
+
+	public function full_form($fname ,$lname ){
+		
+		if (strlen($fname) >0 && ($lname >0))   { 
+		  
+		      	echo 'GOT IT'; 
+		      
+		      } else {  
+		   	echo 'Sorry, Please enter all the info';
+			     }
+		    
+		}
+		public function read_signUpForm() {
+			$row = 1;
+			if (($handle = fopen("bank_file.csv", "r")) !== FALSE) {
+				while (($record = fgetcsv($handle, 0, ",")) !== FALSE) {
+					if($row == 1) {
+						$keys = $record;
+						$row++;
+						print_r($record);
+					} else {
+						$records[] = array_combine($keys, $record);
+					}
+					 
+				}
+				fclose($handle);
+				print_r($records);
+			}
+		
+		}
+	public function write_csv($a){
+		
 	$fp = fopen('bank_file.csv', 'a');
 	
-	foreach ($list as $fields) {
-		$transact = (array) $fields;
-		fputcsv($fp, $transact);
-	}
-	print_r($list);
+		fputcsv($fp, $a);
 	fclose($fp);
 	}
 }
+
+/*	public function read_csv(){
+				
+	}*/
+
 ?>
